3b1c54cd1539bd8fddaefc488fbe0eda44110252
howel <howellj1030@gmail.com>
2025-06-28 10:55:50 +0800

    HW #1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/HW.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/HW.java
@@ -5,10 +5,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class DriveHW extends LinearOpMode{
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotor motor = hardwareMap.dcMotor.get("fl");
-        DcMotor motor = hardwareMap.dcMotor.get("fr");
-        DcMotor motor = hardwareMap.dcMotor.get("bl");
-        DcMotor motor = hardwareMap.dcMotor.get("br");
+        DcMotor fl = hardwareMap.dcMotor.get("fl");
+        DcMotor fr = hardwareMap.dcMotor.get("fr");
+        DcMotor bl = hardwareMap.dcMotor.get("bl");
+        DcMotor br = hardwareMap.dcMotor.get("br");
 
         double y = -gamepad1.left_stick_y;
         double x = gamepad1.left_stick_x;
